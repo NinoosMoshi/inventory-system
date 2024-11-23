fbbb86c9668d1220ef3dfc45855376d230d8e5a8
NinoosMoshi <nalkaton@gmail.com>
2024-11-23 09:15:51 -0600

    add security classes

---
+++ backEnd/inventory-system/src/main/java/com/ninos/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.ninos.security;
+
+import com.ninos.models.User;
+import com.ninos.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Email not found with:  " + email));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
